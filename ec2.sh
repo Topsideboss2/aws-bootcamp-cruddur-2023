bad7e1eb2723ad96449c512e12af7dcf8a88c9b5
root <root@ip-172-31-3-136.ec2.internal>
2023-02-26 13:15:16 +0000

    Modified e2.sh

--- ec2.sh
+++ ec2.sh
@@ -3,14 +3,21 @@ cd /home/ubuntu/
 echo "Now in home directory"
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 echo "Installed curl"
-apt install unzip
+apt install unzip -y
 echo "Installed unzip"
 unzip awscliv2.zip
 echo "awscliv2.zip is unzipped"
 rm awscliv2.zip
+cd /home/ubuntu/aws-bootcamp-cruddur-2023/backend-flask/
+apt install python3-pip -y
+pip3 install -r requirements.txt
+export FRONTEND_URL="*"
+export BACKEND_URL="*"
+export REACT_APP_BACKEND_URL="http://127.0.0.1:4567"
+echo "Backend initialized"
 cd /home/ubuntu/aws-bootcamp-cruddur-2023/frontend-react-js/
 apt update 
-apt install npm
+apt install npm -y
 npm install
 echo "installed npm so frontend is initialized"
 echo "install docker"
