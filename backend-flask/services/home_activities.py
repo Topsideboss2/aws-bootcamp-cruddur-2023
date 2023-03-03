c9a30728bbfefa46987e87b1c14c31703441bc57
Mark Kibara <markkibara2014@gmail.com>
2023-03-03 20:21:53 +0300

    Sync my xray repo with Andrew's xray repo

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,19 +1,18 @@
 from datetime import datetime, timedelta, timezone
-
-# Honeycomb (Acquiting a Tracer)
 from opentelemetry import trace
+
 tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
   def run():
-    with tracer.start_as_current_span("home-activities-mock-data"):
+    with tracer.start_as_current_span("home-activites-mock-data"):
       span = trace.get_current_span()
       now = datetime.now(timezone.utc).astimezone()
       span.set_attribute("app.now", now.isoformat())
       results = [{
         'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
         'handle':  'Andrew Brown',
-        'message': 'Cloud is fun!',
+        'message': 'Cloud is very fun!',
         'created_at': (now - timedelta(days=2)).isoformat(),
         'expires_at': (now + timedelta(days=5)).isoformat(),
         'likes_count': 5,
@@ -30,24 +29,24 @@ class HomeActivities:
           'created_at': (now - timedelta(days=2)).isoformat()
         }],
       },
-      {
-        'uuid': '66e12864-8c26-4c3a-9658-95a10f8fea67',
-        'handle':  'Worf',
-        'message': 'I am out of prune juice',
-        'created_at': (now - timedelta(days=7)).isoformat(),
-        'expires_at': (now + timedelta(days=9)).isoformat(),
-        'likes': 0,
-        'replies': []
-      },
-      {
-        'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-        'handle':  'Garek',
-        'message': 'My dear doctor, I am just simple tailor',
-        'created_at': (now - timedelta(hours=1)).isoformat(),
-        'expires_at': (now + timedelta(hours=12)).isoformat(),
-        'likes': 0,
-        'replies': []
-      }
+        {
+          'uuid': '66e12864-8c26-4c3a-9658-95a10f8fea67',
+          'handle':  'Worf',
+          'message': 'I am out of prune juice',
+          'created_at': (now - timedelta(days=7)).isoformat(),
+          'expires_at': (now + timedelta(days=9)).isoformat(),
+          'likes': 0,
+          'replies': []
+        },
+        {
+          'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
+          'handle':  'Garek',
+          'message': 'My dear doctor, I am just simple tailor',
+          'created_at': (now - timedelta(hours=1)).isoformat(),
+          'expires_at': (now + timedelta(hours=12)).isoformat(),
+          'likes': 0,
+          'replies': []
+        }
       ]
-      span.set_attribute("app-result-length", len(results))
+      span.set_attribute("app.result_length", len(results))
       return results
