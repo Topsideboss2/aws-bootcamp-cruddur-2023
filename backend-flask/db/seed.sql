3dd71a26dc4e43b887f3b411b69d6f3d0d5d7b43
Mark Kibara <markkibara2014@gmail.com>
2023-03-18 09:46:25 +0000

    Modified seed.sql

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,12 +2,13 @@
 INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
   ('Andrew Brown', 'andrewbrown' , 'andrew@exampro.co' , 'MOCK'),
+  ('Mark Kibara', 'topsideboss2', 'markkibara2014@gmail.com', 'MOCK'),
   ('Andrew Bayko', 'bayko' , 'andrewbayko@exampro.co' , 'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
-    'This was imported as seed data!',
+    (SELECT uuid from public.users WHERE users.handle = 'topsideboss2' LIMIT 1),
+    'Hey everyone, welcome to my Cruddur Web App!',
     current_timestamp + interval '10 day'
   )
