190d5994f2d0937d10ebb2c26c4e53375be64f09
Mark Kibara <markkibara2014@gmail.com>
2023-03-30 02:02:04 +0300

    Modified ec2.sh

--- ec2.sh
+++ ec2.sh
@@ -1,25 +1,41 @@
 #!/usr/bin/bash
+
+# Go to /home/ubuntu/ and insall aws cli
 cd /home/ubuntu/
 CYAN='\033[1;36m'
 NO_COLOR='\033[0m'
 LABEL="Now in home directory"
 printf "${CYAN}== ${LABEL}${NO_COLOR}\n"
+
+# Install aws cli
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 CYAN='\033[1;36m'
 NO_COLOR='\033[0m'
-LABEL="Installed curl"
+LABEL="Installed aws cli"
 printf "${CYAN}== ${LABEL}${NO_COLOR}\n"
+
+# install unzip
 apt install unzip -y
 CYAN='\033[1;36m'
 NO_COLOR='\033[0m'
-LABEL="Installed unzip"
+LABEL="install unzip"
 printf "${CYAN}== ${LABEL}${NO_COLOR}\n"
+
+# awscliv2.zip is unzipped
 unzip awscliv2.zip
 CYAN='\033[1;36m'
 NO_COLOR='\033[0m'
 LABEL="awscliv2.zip is unzipped"
 printf "${CYAN}== ${LABEL}${NO_COLOR}\n"
+
+# awscliv2.zip is unzipped
 rm awscliv2.zip
+CYAN='\033[1;36m'
+NO_COLOR='\033[0m'
+LABEL="awscliv2.zip is unzipped"
+printf "${CYAN}== ${LABEL}${NO_COLOR}\n"
+
+# Initialize Backend
 cd /home/ubuntu/aws-bootcamp-cruddur-2023/backend-flask/
 apt install python3-pip -y
 pip3 install -r requirements.txt
@@ -29,25 +45,27 @@ CYAN='\033[1;36m'
 NO_COLOR='\033[0m'
 LABEL="Backend initialized"
 printf "${CYAN}== ${LABEL}${NO_COLOR}\n"
+
+# Initialize Frontend
 cd /home/ubuntu/aws-bootcamp-cruddur-2023/frontend-react-js/
-export REACT_APP_BACKEND_URL="http://54.146.90.96:4567"
-apt update 
+export REACT_APP_BACKEND_URL="http://$(curl -s ifconfig.me):4567"
+apt update
 apt install npm -y
 npm install
 CYAN='\033[1;36m'
 NO_COLOR='\033[0m'
 LABEL="installed npm so frontend is initialized"
 printf "${CYAN}== ${LABEL}${NO_COLOR}\n"
-CYAN='\033[1;36m'
-NO_COLOR='\033[0m'
-LABEL="install docker"
-printf "${CYAN}== ${LABEL}${NO_COLOR}\n"
+
+# install docker
 cd /home/ubuntu/aws-bootcamp-cruddur-2023/
 CYAN='\033[1;36m'
 NO_COLOR='\033[0m'
 LABEL="Installing docker"
 printf "${CYAN}== ${LABEL}${NO_COLOR}\n"
 apt install docker.io
+
+# install docker-compose
 CYAN='\033[1;36m'
 NO_COLOR='\033[0m'
 LABEL="installing docker-compose"
