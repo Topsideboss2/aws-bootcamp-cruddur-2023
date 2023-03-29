4a6b2ce6d0e10384b175cf5252e74fe6d89f374a
Mark Kibara <markkibara2014@gmail.com>
2023-03-29 12:42:11 +0300

    Modified ec2.sh

--- ec2.sh
+++ ec2.sh
@@ -1,30 +1,57 @@
 #!/usr/bin/bash
 cd /home/ubuntu/
-echo "Now in home directory"
+CYAN='\033[1;36m'
+NO_COLOR='\033[0m'
+LABEL="Now in home directory"
+printf "${CYAN}== ${LABEL}${NO_COLOR}\n"
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-echo "Installed curl"
+CYAN='\033[1;36m'
+NO_COLOR='\033[0m'
+LABEL="Installed curl"
+printf "${CYAN}== ${LABEL}${NO_COLOR}\n"
 apt install unzip -y
-echo "Installed unzip"
+CYAN='\033[1;36m'
+NO_COLOR='\033[0m'
+LABEL="Installed unzip"
+printf "${CYAN}== ${LABEL}${NO_COLOR}\n"
 unzip awscliv2.zip
-echo "awscliv2.zip is unzipped"
+CYAN='\033[1;36m'
+NO_COLOR='\033[0m'
+LABEL="awscliv2.zip is unzipped"
+printf "${CYAN}== ${LABEL}${NO_COLOR}\n"
 rm awscliv2.zip
 cd /home/ubuntu/aws-bootcamp-cruddur-2023/backend-flask/
 apt install python3-pip -y
 pip3 install -r requirements.txt
 export FRONTEND_URL="*"
 export BACKEND_URL="*"
-echo "Backend initialized"
+CYAN='\033[1;36m'
+NO_COLOR='\033[0m'
+LABEL="Backend initialized"
+printf "${CYAN}== ${LABEL}${NO_COLOR}\n"
 cd /home/ubuntu/aws-bootcamp-cruddur-2023/frontend-react-js/
 export REACT_APP_BACKEND_URL="http://3.220.231.46:4567"
 apt update 
 apt install npm -y
 npm install
-echo "installed npm so frontend is initialized"
-echo "install docker"
+CYAN='\033[1;36m'
+NO_COLOR='\033[0m'
+LABEL="installed npm so frontend is initialized"
+printf "${CYAN}== ${LABEL}${NO_COLOR}\n"
+CYAN='\033[1;36m'
+NO_COLOR='\033[0m'
+LABEL="install docker"
+printf "${CYAN}== ${LABEL}${NO_COLOR}\n"
 cd /home/ubuntu/aws-bootcamp-cruddur-2023/
-echo "Installing docker"
+CYAN='\033[1;36m'
+NO_COLOR='\033[0m'
+LABEL="Installing docker"
+printf "${CYAN}== ${LABEL}${NO_COLOR}\n"
 apt install docker.io
-echo "installing docker-compose"
+CYAN='\033[1;36m'
+NO_COLOR='\033[0m'
+LABEL="installing docker-compose"
+printf "${CYAN}== ${LABEL}${NO_COLOR}\n"
 curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
