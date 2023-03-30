cee630e7540560cafc0282c980213abb694b38eb
Mark Kibara <markkibara2014@gmail.com>
2023-03-30 22:11:57 +0300

    Add MessageGroupNewPage

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -1,25 +1,25 @@
 import './App.css';
 
 import HomeFeedPage from './pages/HomeFeedPage';
-import NotificationsFeedPage from './pages/NotificationsFeedPage.js';
+import NotificationsFeedPage from './pages/NotificationsFeedPage';
 import UserFeedPage from './pages/UserFeedPage';
 import SignupPage from './pages/SignupPage';
 import SigninPage from './pages/SigninPage';
 import RecoverPage from './pages/RecoverPage';
 import MessageGroupsPage from './pages/MessageGroupsPage';
 import MessageGroupPage from './pages/MessageGroupPage';
+import MessageGroupNewPage from './pages/MessageGroupNewPage';
 import ConfirmationPage from './pages/ConfirmationPage';
 import React from 'react';
-import process from 'process';
-import { Amplify } from 'aws-amplify';
 import {
   createBrowserRouter,
   RouterProvider
 } from "react-router-dom";
 
+import { Amplify } from 'aws-amplify';
+
 Amplify.configure({
   "AWS_PROJECT_REGION": process.env.REACT_APP_AWS_PROJECT_REGION,
-  "aws_cognito_identity_pool_id": process.env.REACT_APP_AWS_COGNITO_IDENTITY_POOL_ID,
   "aws_cognito_region": process.env.REACT_APP_AWS_COGNITO_REGION,
   "aws_user_pools_id": process.env.REACT_APP_AWS_USER_POOLS_ID,
   "aws_user_pools_web_client_id": process.env.REACT_APP_CLIENT_ID,
@@ -51,6 +51,10 @@ const router = createBrowserRouter([
     element: <MessageGroupsPage />
   },
   {
+    path: "/messages/new/:handle",
+    element: <MessageGroupNewPage />
+  },
+  {
     path: "/messages/:message_group_uuid",
     element: <MessageGroupPage />
   },
@@ -74,9 +78,9 @@ const router = createBrowserRouter([
 
 function App() {
   return (
-    <>
-      <RouterProvider router={router} />
-    </>
+      <>
+        <RouterProvider router={router} />
+      </>
   );
 }
 
