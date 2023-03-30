78463d2189aded669b3ccb50c2f2c61b0b2cd45f
Mark Kibara <markkibara2014@gmail.com>
2023-03-30 12:45:23 +0000

    Added into seed.sql

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,6 +2,7 @@
 INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
   ('Andrew Brown', 'andrewbrown' , 'andrew@exampro.co' , 'MOCK'),
+  ('Mark Kibara', 'topsideboss2', 'markkibara2014@gmail.com', 'MOCK'),
   ('Andrew Bayko', 'bayko' , 'andrewbayko@exampro.co' , 'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
